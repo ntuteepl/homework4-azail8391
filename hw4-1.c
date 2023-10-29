be73c9dbfc699c4a4c9fe3d42229e0656321e629
azail8391 <146065149+azail8391@users.noreply.github.com>
2023-10-29 08:15:37 +0800

    Update hw4-1.c

--- hw4-1.c
+++ hw4-1.c
@@ -9,12 +9,12 @@ bool isPrime(int num) {
     if (num <= 3) {
         return true;
     }
-    if (num % 2 == 0  num % 3 == 0) {
+    if (num % 2 == 0 || num % 3 == 0) {
         return false;
     }
 
     for (int i = 5; i * i <= num; i += 6) {
-        if (num % i == 0  num % (i + 2) == 0) {
+        if (num % i == 0 || num % (i + 2) == 0) {
             return false;
         }
         else {
@@ -40,6 +40,9 @@ bool isPrime(int num) {
             printf("NO\n");
         }
     }
+
+    return 0;
+}
 
     return 0;
 }
